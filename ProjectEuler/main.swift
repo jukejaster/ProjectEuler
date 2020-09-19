3eee62a9534cf4e21cfc11b5a80d8221fda7da9d
Juke Jaster <justin@justncode.com>
2020-09-18 23:18:47 -0400

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Juke Jaster on 9/18/20.
+//  Copyright © 2020 Juke Jaster. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
